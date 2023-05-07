6d79b948576e6ec2d497c7c1cb076751771e429a
Mohammad Zeeshan <79397940+zeeshankhan00@users.noreply.github.com>
2023-05-07 18:57:08 +0530

    Update A2Q8.py

--- Assingn2/A2Q8.py
+++ Assingn2/A2Q8.py
@@ -9,7 +9,7 @@ xs = [x/10.0 for x in range(-50,50)]
 plt.plot(xs,[normal_pdf(x,sigma = 1) for x in xs], '-', label= 'mu=0, sigma = 1')
 plt.plot(xs,[normal_pdf(x,sigma = 2) for x in xs], '--', label= 'mu=0, sigma = 2')
 plt.plot(xs,[normal_pdf(x,sigma = 0.5) for x in xs], ':', label= 'mu=0, sigma = 0.5')
-plt.plot(xs,[normal_pdf(x,sigma = -1) for x in xs], '-.', label= 'mu=-1, sigma = 1')
+plt.plot(xs,[normal_pdf(x,mu = -1) for x in xs], '-.', label= 'mu=-1, sigma = 1')
 plt.legend()
 plt.title("various normal pdfs")
 plt.show()

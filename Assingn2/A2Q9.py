0fb1ac3e933d5d8106710f32e048e677178a906d
Mohammad Zeeshan <79397940+zeeshankhan00@users.noreply.github.com>
2023-05-07 18:56:43 +0530

    Create A2Q9.py

---
+++ Assingn2/A2Q9.py
@@ -0,0 +1,19 @@
+import math
+SQRT_TWO_PI = math.sqrt(2*math.pi)
+
+def normal_cdf(x:float, mu:float=0, sigma: float =1)-> float:
+  return(1+ math.erf((x-mu)/math.sqrt(2)/sigma))/2
+
+xs = [x/10.0 for x in range(-50,50)]
+
+
+
+import matplotlib.pyplot as plt
+xs = [x/10.0 for x in range(-50,50)]
+plt.plot(xs,[normal_cdf(x,sigma = 1) for x in xs], '-', label= 'mu=0, sigma = 1')
+plt.plot(xs,[normal_cdf(x,sigma = 2) for x in xs], '--', label= 'mu=0, sigma = 2')
+plt.plot(xs,[normal_cdf(x,sigma = 0.5) for x in xs], ':', label= 'mu=0, sigma = 0.5')
+plt.plot(xs,[normal_cdf(x,mu = -1) for x in xs], '-.', label= 'mu=-1, sigma = 1')
+plt.legend(loc=4)
+plt.title("various normal cdfs")
+plt.show()
